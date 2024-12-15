7a204b5e3318c95c9475075b472612d35109fbdc
Hayato Akatsuka <75282341+hayato94087@users.noreply.github.com>
2024-12-15 22:54:52 +0900

    Prisma を Next.js で利用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
 import { HelloUniverseLabel } from "@/components/hello-universe-label";
 import { env } from "@/env";
+import { db } from "@/server/db";
 
-export default function Home() {
+export default async function Home() {
   const url = env.DEBUG_URL;
-
+  const users = await db.user.findMany({
+    select: {
+      id: true,
+      email: true,
+      name: true,
+    },
+  });
   return (
     <main>
       <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
           <HelloUniverseLabel />
           <p className="text-sm text-gray-500">{url}</p>
+          <br />
+          {users.map((user) => (
+            <div key={user.id} className="text-sm text-gray-500">
+              {user.name},{user.email}
+            </div>
+          ))}
         </div>
       </div>
     </main>
