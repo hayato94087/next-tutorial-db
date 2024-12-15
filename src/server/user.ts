96d40f57d3d63ddbdc579c34be8354f9dddf1ec5
Hayato Akatsuka <75282341+hayato94087@users.noreply.github.com>
2024-12-15 23:05:46 +0900

    データアクセスレイヤーを利用

---
+++ src/server/user.ts
@@ -0,0 +1,18 @@
+import "server-only";
+
+import type { User } from "@prisma/client";
+import { db } from "@/server/db";
+
+function createUsersDTO(userData: User) {
+  // ここに認証機能などを追加して、データセットの絞り込みを行う
+  return {
+    id: userData.id,
+    name: userData.name,
+    email: userData.email,
+  };
+}
+
+export async function getUsers() {
+  const users = await db.user.findMany();
+  return users.map((user) => createUsersDTO(user));
+}
